3fd064807f6d27a8a1000ae00286b8bba9ddb55b
Imran Senyonjo <1019654@isd535.org>
2022-02-17 08:28:56 -0600

    did not finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 const DATA_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DATA_TITLE_SELECTOR ='[data-image-role="title"]';
-const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
