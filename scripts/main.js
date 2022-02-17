a1d65a04d256d8d862b8142bb55b0844cca76ad8
Imran Senyonjo <1019654@isd535.org>
2022-02-17 09:42:15 -0600

    DATA to DETAIL

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-const DATA_IMAGE_SELECTOR = '[data-image-role="target"]';
-const DATA_TITLE_SELECTOR ='[data-image-role="title"]';
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR ='[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR); 
+    detailImage.setAttribute('src', 'img/otter3.jpg');
+}
