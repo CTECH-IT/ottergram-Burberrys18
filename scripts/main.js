827717f6855e7cf49498e7016c485c79372ff7c5
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-02-17 10:43:16 -0600

    retrieving data attributes

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,26 @@ const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR ='[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
-function setDetails() {
+function setDetails(imageUrl, titleText) {
     'use strict';
     let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR); 
-    detailImage.setAttribute('src', 'img/otter3.jpg');
+    detailImage.setAttribute('src', imageUrl);
 
     let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-    detailTitle.textContent = 'Little Deuce Coop';
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+    'use strict';
+    return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
