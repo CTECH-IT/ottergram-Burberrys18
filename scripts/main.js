ad1fd5a8e487efe87b83f3f719d6b639fae5a155
Imran Senyonjo <1019654@isd535.org>
2022-02-16 23:08:24 -0600

    ADDED SOME VARIABLES IN SCRIPTS

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,3 @@
-// Put your JavaScript here
+const DATA_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DATA_TITLE_SELECTOR ='[data-image-role="title"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
