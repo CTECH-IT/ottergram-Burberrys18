da6682c7a2f29ed58530c3d85f69e7a71b385553
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-02-23 10:52:33 -0600

    finished the ottergram website with transitions too

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR ='[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
 const ESC_KEY_CODE = 27;
 
 function setDetails(imageUrl, titleText) {
@@ -51,7 +53,13 @@ function hideDetails() {
 
 function showDetails() {
     'use strict';
+    let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
     document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(function () {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+    
 }
 
 function addKeyPressHandler() {
